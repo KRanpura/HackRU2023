d282e5c72b249afe0ee425eeae137cf26f6c1331
Kusum Gandham <koolkusum10@gmail.com>
2023-10-07 15:49:44 -0400

    Sign up page done

--- eventbuddy/client/src/components/Post.js
+++ eventbuddy/client/src/components/Post.js
@@ -1,56 +1,8 @@
-import React, {useState} from 'react';
-import {getUser} from '../api';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-reactor-dom';
 import { useNavigate } from 'react-router-dom';
-function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const nav = useNavigate();
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      try {
-        const response = await getUser(email,password);//if response status code
-        sessionStorage.setItem('email', email);
-        sessionStorage.setItem('password', password);
+import { updatePost } from '../api';
+function Post()
+{
 
-        if (response.status===200)
-        {
-          nav('/StudentPage');
-        }
-        //console.log(response); // do something with the user data, like redirect to a new page
-      } catch (error) {
-        console.error(error); // handle errors
-      }
-    };
-  
-    return (
-      <div className="login">
-         <h1>Login</h1> 
-        <form onSubmit={handleLogin}>
-          <label>
-            {/* Email: */}
-            <input
-              type="email"
-              value={email}
-              placeholder='Email Address'
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </label>
-          <label>
-            {/* Password: */}
-            <input
-              type="password"
-              value={password}
-              placeholder='Password'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button type="submit">Login</button>
-          <p>Don't have an account? <Link to="/Signup">Sign up</Link></p>
-        </form>
-  
-      </div>
-    );
-  }
-  
-  export default Login;
+};
