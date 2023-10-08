392cc256d97894a26190568091df775921fca3a2
Khushi <ranpurakhushi@gmail.com>
2023-10-08 01:45:27 -0400

    calendar

--- eventbuddy/client/src/components/Calendar.js
+++ eventbuddy/client/src/components/Calendar.js
@@ -36,5 +36,102 @@ function Calendar () {
     </div>
     );
 }
+function year_range(from, until) {
+    var years = "";
+    for (var year = start; year <= end; year++) {
+        years += "<option value =" + year + ">" + year + "</option";
+    }
+    return years;
+}  
 
+today = new Date();
+thisMonth = today.getMonth();
+thisYear = today.getFullYear();
+chooseYear = document.getElementById("year");
+chooseMonth = document.getElementById("month");
+
+createYear = generate_year_range(2021, 2024);
+
+document.getElementById("year").innerHTML = createYear;
+
+var calendar = document.getElementById("calendar");
+
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+var $dataHead = "<tr>";
+for (dhead in days) {
+    $dataHead += "<th data-days='" + days[dhead] + "'>" + days[dhead] + "</th>";
+}
+$dataHead += "</tr>";
+
+document.getElementById("thead-month").innerHTML = $dataHead;
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(thisMonth, thisYear);
+
+function next() {
+    thisYear = (thisMonth === 11) ? thisYear + 1 : thisYear;
+    thisMonth = (thisMonth + 1) % 12;
+    showCalendar(thisMonth, thisYear);
+}
+
+function previous() {
+    thisYear = (thisMonth === 0) ? thisYear - 1 : thisYear;
+    thisMonth = (thisMonth === 0) ? 11 : thisMonth - 1;
+    showCalendar(thisMonth, thisYear);
+}
+
+function jump() {
+    thisYear = parseInt(chooseYear.value);
+    thisMonth = parseInt(chooseMonth.value);
+    showCalendar(thisMonth, thisYear);
+}
+
+function displayCalendar(month, year) {
+
+    var firstDay = (new Date( year, month ) ).getDay();
+
+    table = document.getElementById("calendar-body");
+
+    table.innerHTML = "";
+
+    monthAndYear.innerHTML = months[month] + " " + year;
+    chooseYear.value = year;
+    chooseMonth.value = month;
+
+    date = 1; //formatting all date boxes in the calendar
+    for (i = 0; i < 6; i++ ) {
+        
+        row = document.createElement("tr");
+        for (j = 0; j < 7; j++ ) {
+            if ( i === 0 && j < firstDay ) {
+                box = document.createElement( "td" );
+                boxText = document.createTextNode("");
+                box.appendChild(boxText);
+                row.appendChild(box);
+            } 
+            else if (date > daysInMonth(month, year)) {
+                break;
+            } 
+            else {
+                box = document.createElement("td");
+                box.setAttribute("data-date", date);
+                box.setAttribute("data-month", month + 1);
+                box.setAttribute("data-year", year);
+                box.setAttribute("data-month_name", months[month]);
+                box.className = "date-picker";
+                box.innerHTML = "<span>" + date + "</span>";
+
+                if ( date === today.getDate() && year === today.getFullYear() && month === today.getMonth() ) {
+                    cell.className = "date-picker selected";
+                }
+                row.appendChild(cell);
+                date++;
+            }
+        }
+        tbl.appendChild(row);
+    }
+}
 export default Calendar;
